fa025e8176a0178120d6071e7fd126e5bf7c0b6c
caduunisal <kadu.unisal1@gmail.com>
2024-07-26 17:54:45 -0300

    First commit

---
+++ Chatbot.py
@@ -0,0 +1,16 @@
+class Chatbot():
+    def __init__(self, nome):
+        print(f"Bem vindo ao Chatbot, {nome.title()}!")
+        
+    def fala(self):
+        print("Oi")
+    
+    def sai(self):
+        print("Tchau!")
+    
+a = Chatbot("carlos")
+a.fala()
+a.sai()
+
+
+# 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
