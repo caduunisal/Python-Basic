2d730b8941f2958a2f25e49d551d7dda3e2dd4c8
caduunisal <kadu.unisal1@gmail.com>
2024-07-26 17:19:16 -0300

    Initial version

---
+++ Chatbot.py
@@ -0,0 +1,13 @@
+class Chatbot():
+    def __init__(self, nome):
+        print(f"Bem vindo ao Chatbot, {nome.title()}!")
+        
+    def fala(self):
+        print("Oi")
+    
+    def sai(self):
+        print("Tchau!")
+    
+a = Chatbot("carlos")
+a.fala()
+a.sai()
